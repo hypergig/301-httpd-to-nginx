437c5d484b2bd2110d833d58e9019ab06967a02e
Jordan Cohen <hypergig@gmail.com>
2016-02-10 08:33:12 -0500

    test environment refactor

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 # /bin/bash
 set -e
-echo "Running environment..."
+echo "converting htaccess file..."
+docker run -it --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp python:2 python 301-converter.py
+echo "Running test environment..."
 docker-compose rm -f
 docker-compose build
 docker-compose up
--- test.sh
+++
@@ -1,6 +0,0 @@
-# /bin/bash
-set -e
-echo "Running environment..."
-docker-compose rm -f
-docker-compose build
-docker-compose up
